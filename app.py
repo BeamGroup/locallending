c468ac155d0f49e80e14e14db597cf0ee0189073
xChenny <andrew.chen923@gmail.com>
2017-10-29 02:16:48 -0400

    implemented routing

--- app.py
+++ app.py
@@ -7,3 +7,14 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+@app.route('/search')
+def search():
+    return render_template('search.html')
+
+@app.route('/results')
+def results():
+    return render_template('results.html')
