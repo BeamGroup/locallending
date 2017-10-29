1e5dc21e8fad215f28de44d5a0eff44d33084370
xChenny <andrew.chen923@gmail.com>
2017-10-29 17:05:56 -0400

    added in default owner id

--- new_item.py
+++ new_item.py
@@ -1,14 +1,14 @@
 import mysql.connector
 
-def new_item(item_name,description,category,owner, holder):
+def new_item(item_name,description,category,owner, holder, owner_id='1'):
 	cnx = mysql.connector.connect(user='root', password='bensommer',
                               host='127.0.0.1',
                               database='locallending')
 	cursor = cnx.cursor()
 
 	add_item = ("INSERT INTO items "
-               "(item_name,description, category,owner_username,holder_username )"
-               "VALUES (%s, %s, %s, %s, %s)")
+               "(item_name,description, category,owner_username,holder_username, owner_id)"
+               "VALUES (%s, %s, %s, %s, %s, %s)")
 
 	item_data = (item_name,description,category, owner, holder)
 
