7ad2cf7712d42ed0e3bb9c1fd45aef302df8d220
Michael Kolber <michaelkolber@users.noreply.github.com>
2017-10-29 04:28:42 -0400

    updated search.js

---
+++ static/js/search.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    var hidden = true;
+    $('#cat-toggle').click(function() {
+        if (hidden === true) {
+            $('#search-cats').css({display:''});
+            hidden = false;
+        }
+        else if (hidden === false) {
+            $('#search-cats').css({display:'none'});
+            hidden = true;
+        }
+        else {
+            console.log('ERROR: Cat toggle variable is neither "true" nor "false".');
+        }
+    });
+});
