d7a866900f5ca07cf2cf7d0bb7305148175a0677
ericshermancs <zensap92@gmail.com>
2017-10-31 21:54:41 -0400

    testing logged in as

--- trade.php
+++ trade.php
@@ -23,6 +23,7 @@ if($_SESSION['username'] == null){
 			<div id="wrapper">
 				<!-- Header -->
 					<header id="header">
+						<p>Logged in as <?php echo $_SESSION['username']; ?></p>
 						<?php 
 						include 'connect.php';
 						$sql = ("SELECT item_name, holder_username FROM `items` WHERE item_id ='".$_GET['item_id']."'");
