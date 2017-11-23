d53581dec91db71bc14684c0bff69697c68f0633
Michael Kolber <michaelkolber@users.noreply.github.com>
2017-11-22 21:56:35 -0500

    Made the navbar PHP more easily syntactically readable

--- header.php
+++ header.php
@@ -59,16 +59,17 @@ session_start();
     <body>
         <?php
         if (isset($_SESSION['username'])){
-            echo "<div class=\"navbar\">
-            <a class=\"navbar__name\" href=\"/\">Local Lending</a>
-            <div class=\"navbar__navigation\">
-                <a class=\"navbar__navbutton\" href=\"dashboard.php\">Dashboard</a>
-                <a class=\"navbar__navbutton\" href=\"search.php\">Search</a>
-                <a class=\"navbar__navbutton\" href=\"new_item.php\">New Item</a>
-                <a class=\"navbar__navbutton navbar__navbutton--logout\" href=\"logout.php\">Logout</a>
-            </div>
-        </div>
-        <div class=\"navbar--spacer\"></div>";
-        }
-        
+		?>
+            <div class="navbar">
+            	<a class="navbar__name" href="/">Local Lending</a>
+            	<div class="navbar__navigation">
+                	<a class="navbar__navbutton" href="dashboard.php">Dashboard</a>
+                	<a class="navbar__navbutton" href="search.php">Search</a>
+                	<a class="navbar__navbutton" href="new_item.php">New Item</a>
+                	<a class="navbar__navbutton navbar__navbutton--logout" href="logout.php">Logout</a>
+            	</div>
+        	</div>
+        <?php
+		}
         ?>
+		<div class="navbar--spacer"></div>
